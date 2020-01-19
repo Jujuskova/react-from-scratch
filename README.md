89b9643dad50d9e01a86d3bcb967248502e970df
Julie Lucanik <julie.lucanik@gmail.com>
2020-01-19 12:20:47 +0100

    Set up project with react webpack and babel

---
+++ README.md
@@ -0,0 +1,8 @@
+# Set up React project from scratch
+
+with: 
+ - Webpack
+ - Babel
+ - Eslint / Prettier
+ - Flow
+ - Jest
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'dist/bundle.js',
+  },
+  devServer: {
+    contentBase: './',
+    hot: true,
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        //exclude: /node_modules/,
+        include: [path.resolve(__dirname, './src')],
+        use: {
+          loader: "babel-loader",
+        }
+      }
+    ]
+  },
+
+}
